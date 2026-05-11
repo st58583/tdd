2847645a3bef550ed3cd725fcf0b77ec85b37cb1
st58583 <delta.fixer@seznam.cz>
2026-05-12 00:58:57 +0200

    feat(domain): TDD create Equipment entity
    - REFACTOR: used PHP 8 constructor property promotion

--- src/Equipment.php
+++ src/Equipment.php
@@ -6,15 +6,11 @@ namespace App;
 
 class Equipment
 {
-    private string $name;
-    private string $category;
-    private float $dailyRate;
-
-    public function __construct(string $name, string $category, float $dailyRate)
-    {
-        $this->name = $name;
-        $this->category = $category;
-        $this->dailyRate = $dailyRate;
+    public function __construct(
+        private string $name,
+        private string $category,
+        private float $dailyRate
+    ) {
     }
 
     public function getName(): string
